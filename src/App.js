d3ef5c6b05c568f48d1b239183c5b45c03064e07
lana2-2karla <allanakarla1702@gmail.com>
2022-12-20 19:26:24 -0300

    :ok_hand: style: lint in app,js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={ <Home /> }/>
     </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
