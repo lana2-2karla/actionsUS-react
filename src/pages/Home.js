588d243c2f58f61c23534d61015273d09ebaed80
lana2-2karla <allanakarla1702@gmail.com>
2022-12-20 20:04:16 -0300

    :sparkles: feat: request with axios using useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,17 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
 function Home () {
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
       <div>Homeeeeeee</div>
   )
