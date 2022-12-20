67ae75fcaef8121ca4529697ed443cc5ebf58028
lana2-2karla <allanakarla1702@gmail.com>
2022-12-20 20:08:04 -0300

    :sparkles: feat: create state to coins

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function Home () {
+  const [coins, setCoins] = useState([])
+
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
 
   useEffect(() => {
     axios.get(url).then((response) => {
+      setCoins(response.data)
       console.log(response.data[0])
     }).catch((error) => {
       console.log(error)
