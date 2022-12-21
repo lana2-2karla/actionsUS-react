402fde89486bc6dd38c162e40693c537258004b3
lana2-2karla <allanakarla1702@gmail.com>
2022-12-21 11:30:32 -0300

    :sparkles: feat: arrow actions via props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ActionCard from '../components/ActionCard'
 
 function Home () {
-  // const [coins, setCoins] = useState([])
+  const [actions, setActions] = useState([])
 
   const url = 'https://api.finage.co.uk/last/stocks/?symbols=AAPL,TSLA,GOGL,AMZN,NVDA,META,NU&apikey=API_KEY62QY4RI0VTOJ7IL0SE1K1VGHO7NNBULI'
 
   useEffect(() => {
     axios.get(url).then((response) => {
-      console.log(response)
+      setActions(response.data)
     }).catch((error) => {
       console.log(error)
     })
   }, [])
 
   return (
-    <ActionCard />
+    <ActionCard actionsUS={actions}/>
   )
 }
 
