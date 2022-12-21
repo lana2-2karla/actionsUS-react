8ad66842f8cacacfb92b830a4b8bccd39e7ab75e
lana2-2karla <allanakarla1702@gmail.com>
2022-12-21 13:01:34 -0300

    :recycle: refactor: url api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import ActionCard from '../components/ActionCard'
 function Home () {
   const [actions, setActions] = useState([])
 
-  const url = 'https://api.finage.co.uk/last/stocks/?symbols=AAPL,TSLA,GOGL,AMZN,NVDA,META,NU&apikey=API_KEY62QY4RI0VTOJ7IL0SE1K1VGHO7NNBULI'
+  const url = 'https://api.finage.co.uk/last/stocks/?symbols=AAPL,TSLA,GOGL,AMZN,NVDA,META,NU,INTC&apikey=API_KEY62QY4RI0VTOJ7IL0SE1K1VGHO7NNBULI'
 
   useEffect(() => {
     axios.get(url).then((response) => {
