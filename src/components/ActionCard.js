68ff7850aa2f12a5e0ce02092729808a9b38a3ec
lana2-2karla <allanakarla1702@gmail.com>
2022-12-21 10:55:45 -0300

    :sparkles: feat: action card

---
+++ src/components/ActionCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ActionCard = () => {
+  return (
+        <div>
+            <div>
+                <p>AMZN</p>
+            </div>
+            <p>Abertura $86.47</p>
+            <p>Baixa $86.45</p>
+        </div>
+  )
+}
+
+export default ActionCard
