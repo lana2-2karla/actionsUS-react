545919d751017074faafb97039065d6b8d4b5093
lana2-2karla <allanakarla1702@gmail.com>
2022-12-21 17:26:41 -0300

    :recycle: refactor: use axios extension in request

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import ActionCard from '../components/ActionCard'
 import Header from '../components/Header'
+import { api } from '../service/http'
 
 function Home () {
   const [actions, setActions] = useState([])
 
-  const url = 'https://api.finage.co.uk/last/stocks/?symbols=AAPL,TSLA,GOGL,AMZN,NVDA,META,NU,INTC&apikey=API_KEY62QY4RI0VTOJ7IL0SE1K1VGHO7NNBULI'
-
   useEffect(() => {
-    axios.get(url).then((response) => {
+    api.get().then((response) => {
       setActions(response.data)
     }).catch((error) => {
       console.log(error)
