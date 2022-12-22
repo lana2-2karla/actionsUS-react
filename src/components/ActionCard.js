7e8308198680abc403d67e3b3f055ac83c9b2b01
lana2-2karla <allanakarla1702@gmail.com>
2022-12-21 21:31:32 -0300

    :sparkles: feat: h1 button and function handleClick

--- src/components/ActionCard.js
+++ src/components/ActionCard.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ActionCard.css'
 import PropTypes from 'prop-types'
 
 const ActionCard = ({ actionsUS }) => {
+  const [actions, setActions] = useState(actionsUS)
+
+  const handleClick = async () => {
+    const actionsAlphabeticalOrder = [...actionsUS].sort((a, b) => a.symbol.localeCompare(b.symbol))
+    setActions(actionsAlphabeticalOrder)
+  }
+  console.log(actions)
+
   return (
+    <>
+        <div className='action-container'>
+          <h1>Explore o mercado</h1>
+          <button
+          type='submit'
+          onClick={handleClick}
+          >A-Z</button>
+       </div>
       <div className='action-container'>
-           { actionsUS.map((action, index) => (
+           {
+           actions.map((action, index) => (
             <div key={index} className='action-card'>
               <p className='white'>{action.symbol}</p>
               <p className='white'>Abertura ${action.ask}</p>
@@ -14,6 +31,7 @@ const ActionCard = ({ actionsUS }) => {
            ))
            }
         </div>
+    </>
 
   )
 }
